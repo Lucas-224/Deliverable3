111c166a5768050811f701c57770fda96f0cc53e
Kemon <Kemon@192.168.1.6>
2021-08-14 15:15:45 -0500

    verifying commit

--- delv3/src/HumanPlayer.java
+++ delv3/src/HumanPlayer.java
@@ -13,8 +13,16 @@ public class HumanPlayer extends Player {
     }
 
     public void helpMe() {
-        // TODO - implement HumanPlayer.helpMe
-        throw new UnsupportedOperationException();
+        System.out.println("----------How to Play----------\n");
+        System.out.println("""
+        \u201cWar\u201d is a two-player game wherein players choose a card from 
+        their hand and reveal their values at the same time as one another.  
+
+        Whoever has the highest value card wins the \u201cbattle\u201d, and gets to keep the pair of cards (or, gets a point).  
+
+        If there is a draw, players each choose another card to reveal at the same time, and this continues until one player wins 
+
+        the \u201cbattle\u201d.  Whoever has the most points in the end wins the game of war.""");
     }
 
 }
