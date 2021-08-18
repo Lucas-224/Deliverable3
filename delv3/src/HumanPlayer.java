f1dbd68f95508403f9cb7eacff0326c72ca99745
Chris <Chris@192.168.2.227>
2021-08-17 23:58:07 -0400

    Updated GroupOfCards + more testing. We'll need to address PlayingCard vs. Card later...

--- delv3/src/HumanPlayer.java
+++ delv3/src/HumanPlayer.java
@@ -3,29 +3,12 @@ public class HumanPlayer extends Player {
     public HumanPlayer(String name) {
         super(name);
     }
-
-    public void viewHand() {
-        // TODO - implement HumanPlayer.viewHand
-        throw new UnsupportedOperationException();
-    }
-
+    
     public void surrender() {
         // TODO - implement HumanPlayer.surrender
         throw new UnsupportedOperationException();
     }
 
-    public void helpMe() {
-        System.out.println("----------How to Play----------\n");
-        System.out.println("""
-        \u201cWar\u201d is a two-player game wherein players choose a card from 
-        their hand and reveal their values at the same time as one another.  
-
-        Whoever has the highest value card wins the \u201cbattle\u201d, and gets to keep the pair of cards (or, gets a point).  
-
-        If there is a draw, players each choose another card to reveal at the same time, and this continues until one player wins 
-
-        the \u201cbattle\u201d.  Whoever has the most points in the end wins the game of war.""");
-
+    
     }
 
-}
