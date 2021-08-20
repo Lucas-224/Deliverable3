2b3cde782b088382110b8702dfacdacc06d7bb15
Chris <Chris@192.168.2.227>
2021-08-19 20:45:18 -0400

    JavaDocs

--- delv3/src/Card.java
+++ delv3/src/Card.java
@@ -1,8 +1,13 @@
-
+/**
+ * This class models a generic card.  It has no current function on its own.
+ *
+ * @see {@link PlayingCard}
+ * @author Course Instructor
+ */
 public abstract class Card {
 
     public String toString() {
-        // TODO - implement Card.toString
+        // left purposefully blank...override in child class!
         throw new UnsupportedOperationException();
     }
 
